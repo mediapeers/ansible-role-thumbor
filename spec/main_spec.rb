51a029d7884528086463f37e7fe4dae21ae8d2a7
Stefan Horning <stefan@hornings.de>
2016-01-22 17:06:54 +0100

    Setup serverspec to run as part of travis build

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -2,6 +2,54 @@ require 'spec_helper'
 
 describe 'Thumbor setup' do
 
+  describe package('supervisor') do
+    it { should be_installed }
+  end
+
+  describe package('python-pip') do
+    it { should be_installed }
+  end
+
+  describe package('python-dev') do
+    it { should be_installed }
+  end
+
+  describe package('libcurl4-openssl-dev') do
+    it { should be_installed }
+  end
+
+  describe package('python-pgmagick') do
+    it { should be_installed }
+  end
+
+  describe package('libjpeg-dev') do
+    it { should be_installed }
+  end
+
+  describe package('libtiff-dev') do
+    it { should be_installed }
+  end
+
+  describe package('libpng-dev') do
+    it { should be_installed }
+  end
+
+  describe package('libjasper-dev') do
+    it { should be_installed }
+  end
+
+  describe package('libwebp-dev') do
+    it { should be_installed }
+  end
+
+  describe package('tc_aws') do
+    it { should be_installed.by('pip') }
+  end
+
+  describe package('certifi') do
+    it { should be_installed.by('pip').with_version('2015.04.28') }
+  end
+
   describe file('/etc/thumbor/thumbor.conf') do
     it { should be_file}
   end
