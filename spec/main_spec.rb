5d1bbc0d80f13566759620050dedecd98b0881e3
Stefan Horning <stefan@hornings.de>
2016-01-24 22:01:05 +0100

    Far more specs

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -47,14 +47,51 @@ describe 'Thumbor setup' do
   end
 
   describe package('certifi') do
-    # somehow doesn't work with version check
-#    it { should be_installed.by('pip').with_version('2015.04.28') }
-    it { should be_installed.by('pip') }
+    it { should be_installed.by('pip').with_version('2015.4.28') }
   end
 
-  describe file('/etc/thumbor/thumbor.conf') do
-    it { should be_file}
+  config_dir = ANSIBLE_VARS.fetch('thumbor_config_dir', '/')
+
+  describe file("#{config_dir}/thumbor.conf") do
+    it { should be_file }
+    its(:content) { should include("#{ANSIBLE_VARS.fetch('thumbor_bucket_prefix', 'FAIL')}.*s3.amazonaws.com") }
+    its(:content) { should include("TC_AWS_REGION = '#{ANSIBLE_VARS.fetch('s3_aws_region', 'FAIL')}'") }
   end
 
-  # TODO: more testing
+  describe file("#{config_dir}/thumbor.key") do
+    it { should be_file }
+    its(:content) { should eq "#{ANSIBLE_VARS.fetch('thumbor_signing_key', 'FAIL')}" }
+  end
+
+  describe file("#{ANSIBLE_VARS.fetch('thumbor_log_dir', 'FAIL')}") do
+    it { should be_directory }
+  end
+
+  describe file("#{ANSIBLE_VARS.fetch('supervisord_log_dir', 'FAIL')}") do
+    it { should be_directory }
+  end
+
+  describe file('/etc/supervisor/supervisord.conf') do
+    it { should be_file }
+    its(:content) { should include "numprocs=#{ANSIBLE_VARS.fetch('thumbor_processes', 'FAIL')}" }
+  end
+
+  describe service('supervisor') do
+    it { should be_started }
+  end
+
+  # Quick smoketest of Nginx setup (nginx role should be tested itself already):
+  describe file('/etc/nginx/nginx.conf') do
+    it { should be_file }
+  end
+
+  describe file('/etc/nginx/sites-available/default') do
+    it { should be_file }
+    its(:content) { should include "proxy_pass http://thumbor;" }
+  end
+
+  describe file('/etc/nginx/sites-enabled/default') do
+    it { should be_symlink }
+  end
+
 end
