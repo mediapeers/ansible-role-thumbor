5ec2b7ab499eb648054a8b90b91d63ac83e3b0c4
Stefan Horning <stefan@hornings.de>
2016-01-26 17:12:10 +0100

    Ensure thumbor user is actually created when one is configured that doesn't exist yet. Also more specs. Dropped graylog config from ngxinx params

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -50,35 +50,58 @@ describe 'Thumbor setup' do
     it { should be_installed.by('pip').with_version('2015.4.28') }
   end
 
+  describe user(ANSIBLE_VARS.fetch('thumbor_user', 'FAIL')) do
+    it { should exist }
+    it { should belong_to_group(ANSIBLE_VARS.fetch('thumbor_user', 'FAIL')) }
+  end
+
   config_dir = ANSIBLE_VARS.fetch('thumbor_config_dir', '/')
 
   describe file("#{config_dir}/thumbor.conf") do
     it { should be_file }
+    it { should be_owned_by('root') }
+    it { should be_grouped_into(ANSIBLE_VARS.fetch('thumbor_user', 'FAIL')) }
+    it { should be_mode(640) }
     its(:content) { should include("#{ANSIBLE_VARS.fetch('thumbor_bucket_prefix', 'FAIL')}.*s3.amazonaws.com") }
     its(:content) { should include("TC_AWS_REGION = '#{ANSIBLE_VARS.fetch('s3_aws_region', 'FAIL')}'") }
   end
 
   describe file("#{config_dir}/thumbor.key") do
     it { should be_file }
+    it { should be_owned_by('root') }
+    it { should be_grouped_into(ANSIBLE_VARS.fetch('thumbor_user', 'FAIL')) }
+    it { should be_mode(640) }
     its(:content) { should eq "#{ANSIBLE_VARS.fetch('thumbor_signing_key', 'FAIL')}\n" }
   end
 
   describe file("#{ANSIBLE_VARS.fetch('thumbor_log_dir', 'FAIL')}") do
     it { should be_directory }
+    it { should be_mode(755) }
   end
 
   describe file("#{ANSIBLE_VARS.fetch('supervisord_log_dir', 'FAIL')}") do
     it { should be_directory }
+    it { should be_mode(755) }
   end
 
   describe file('/etc/supervisor/supervisord.conf') do
     it { should be_file }
     its(:content) { should include "numprocs=#{ANSIBLE_VARS.fetch('thumbor_processes', 'FAIL')}" }
+    its(:content) { should include "user=#{ANSIBLE_VARS.fetch('thumbor_user', 'FAIL')}" }
   end
 
-#  describe service('supervisor') do
-#    it { should be_started }
-#  end
+  describe service('supervisor') do
+    it { should be_enabled }
+#    it { should be_running }
+  end
+
+  describe port(8001) do
+    it { should be_listening.on('127.0.0.1').with('tcp') }
+  end
+
+  describe port(80) do
+    it { should be_listening.on('127.0.0.1').with('tcp') }
+  end
 
   # Quick smoketest of Nginx setup (nginx role should be tested itself already):
   describe file('/etc/nginx/nginx.conf') do
